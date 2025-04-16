96e10a80b94d4f087bc8920c2469a06e9d459d11
sarathmohan.devops <sarathkumar.mohan6@gmail.com>
2025-04-16 12:59:47 -0300

    adding release worflow and s3site module

---
+++ s3-site/variable.tf
@@ -0,0 +1,24 @@
+variable "region" {
+    type = string
+}
+variable "bucket" {
+    type = string
+}
+variable "index_location" {
+    type = string
+}
+variable "index_key" {
+    type = string
+}
+variable "index_object_acl" {
+    type = string
+}
+variable "supporting_file_location" {
+    type = string
+}
+variable "supporting_file_key" {
+    type = string
+}
+variable "supporting_file_object_acl" {
+    type = string
+}
